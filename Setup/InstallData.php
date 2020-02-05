ee25cf528a888fd7ff6f9c555967c85e5e3d3388
Elton Doehnert <eltonfd@gmail.com>
2020-02-05 10:49:35 -0300

    first commit

---
+++ Setup/InstallData.php
@@ -0,0 +1,77 @@
+<?php
+namespace Vexpro\PontosProduto\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use \Magento\Catalog\Model\Product;
+use \Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Config;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;    
+
+    public function __construct(
+        EavSetupFactory $eavSetupFactory,
+        Config $eavConfig,
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
+    )
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->eavConfig       = $eavConfig;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;        
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'pontos_produto',
+            [
+                'type' => 'decimal',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'pontos_produto',
+                'input' => 'text',
+                'class' => '',
+                'source' => '',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => true,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => 'simple',
+            ]
+        );
+
+         $setup->endSetup();
+
+    }
+}
